91a02261275a7686b0e5fed84042817ec7d4bbd7
thierryleung <ahkhiat@hotmail.com>
2025-04-04 12:04:05 +0200

    update user

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Security\Core\Security;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,13 +13,63 @@ use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
 class UserController extends AbstractController
 {
     #[Route('/api/user/infos', name: 'api_user_info', methods: ['GET'])]
-    public function getUserInfo(Request $request, JWTTokenManagerInterface $jwtManager): JsonResponse
+    public function getUserInfo(
+        Request $request, 
+        // JWTTokenManagerInterface $jwtManager
+        ): JsonResponse
     {
         $user = $this->getUser();
 
         if (!$user) {
             return new JsonResponse(['error' => 'User not authenticated'], JsonResponse::HTTP_UNAUTHORIZED);
         }
+
+        return new JsonResponse([
+            'id' => $user->getId(),
+            'email' => $user->getEmail(),
+            'firstname' => $user->getFirstname(),
+            'lastname' => $user->getLastname(),
+            'birthdate' => $user->getBirthdate() ? $user->getBirthdate()->format('Y-m-d') : null,
+            'city' => $user->getCity()
+        ]);
+    }
+
+    #[Route('/api/user/update', name: 'api_user_update', methods: ['PUT'])]
+    public function updateUser(
+        Request $request, 
+        EntityManagerInterface $entityManager
+        ): JsonResponse
+    {
+        $user = $this->getUser();
+
+        if (!$user) {
+            return new JsonResponse(['error' => 'User not authenticated'], JsonResponse::HTTP_UNAUTHORIZED);
+        }
+        $data = json_decode($request->getContent(), true);
+        if (!$data) {
+            return new JsonResponse(['error' => 'Invalid JSON'], JsonResponse::HTTP_BAD_REQUEST);
+        }
+        if (isset($data['email'])) {
+            $user->setEmail($data['email']);
+        }
+        if (isset($data['firstname'])) {
+            $user->setFirstname($data['firstname']);
+        }
+        if (isset($data['lastname'])) {
+            $user->setLastname($data['lastname']);
+        }
+        if (isset($data['birthdate'])) {
+            try {
+                $user->setBirthdate(new \DateTime($data['birthdate']));
+            } catch (\Exception $e) {
+                return new JsonResponse(['error' => 'Invalid date format'], JsonResponse::HTTP_BAD_REQUEST);
+            }
+        }
+        if (isset($data['city'])) {
+            $user->setCity($data['city']);
+        }
+        $entityManager->persist($user);
+        $entityManager->flush();
 
         return new JsonResponse([
             'id' => $user->getId(),
